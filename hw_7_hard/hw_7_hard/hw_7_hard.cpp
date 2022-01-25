ac66c8d9992366f66961da59ed0418b8d56fdaa4
NikolayPhilippov <n.philippov@gmail.com>
2022-01-26 01:17:56 +0300

    7 !!!

--- hw_7_hard/hw_7_hard/hw_7_hard.cpp
+++ hw_7_hard/hw_7_hard/hw_7_hard.cpp
@@ -1,20 +1,96 @@
-﻿// hw_7_hard.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#pragma once
 
 #include <iostream>
+#include <cmath>
+#include <map>
+#include <utility>
+#include <fstream>
+#include <string>
+#include <set>
+#include <list>
+#include <vector>
+#include "part2.pb.h"
+
+class IRepository {
+	virtual void Open() = 0; // бинарная десериализация в файл
+	virtual void Save() = 0; // бинарная сериализация в файл
+};
+
+class IMethods {
+	virtual double GetAverageScore(const FullName& name) = 0;
+	virtual std::string GetAllInfo(const FullName& name) = 0;
+	virtual std::string GetAllInfo() = 0;
+};
+
+class STGroups : public IRepository, public IMethods
+{
+public:
+
+    STGroups() = default;
+    STGroups(const std::string& fname) : fileName(fname) {};
+
+    virtual void Open() override;
+    virtual void Save() override;
+
+    virtual double GetAverageScore(const FullName& name) override;
+    virtual std::string GetAllInfo(const FullName& name) override;
+    virtual std::string GetAllInfo() override;
+
+    bool operator == (const FullName& p1, const FullName& p2);
+
+
+private:
+    std::string fileName{ "StudentsGroups.bin" };
+    StudentsGroup stGrups;
+};
+
+void STGroups::Open()
+{
+    std::ifstream file{ fileName, std::ios_base::binary };
+
+    if (!file.is_open()) {
+        std::cout << "Ошибка открытия файла" << std::endl;
+        return;
+    }
+    else {
+        std::cout << "Файл открыт на чтение" << std::endl;
+    }
+};
+
+void STGroups::Save()
+{
+    std::ofstream file{ fileName, std::ios_base::binary };
+
+    if (!file.is_open()) {
+        std::cout << "Ошибка сохранения файла" << std::endl;
+        return;
+    }
+    else {
+        file.close();
+        std::cout << "Файл закрыт " << std::endl;
+    }
+};
+
+bool STGroups::operator == (const FullName& p1, const FullName& p2)
+{
+    return std::tie(p1.name(), p1.surname(), p1.patronymic()) ==
+        std::tie(p2.name(), p2.surname(), p2.patronymic());
+}
+
+double STGroups::GetAverageScore(const FullName& name)
+{
+    for ( ; ; ) {
+    {   
+        // проверка по итератору и вывод информации
+
+        if (name == w ) {
+
+        }
+            
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "rus");
+
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
